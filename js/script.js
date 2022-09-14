6e713fb9b619e19fb6d5365ecbef293a894c36e6
Margarita <marodevalois@gmail.com>
2022-09-14 17:29:29 +0200

    nethods tasks

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
 // Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 // 2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
 // переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+// 3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+// Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+// при помощи метода forEach вывести в консоль сообщения в таком виде:
+// "Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
 'use strict';
 
@@ -47,17 +51,26 @@ const personalMovieDB = {
             console.log(personalMovieDB);
         }
     },
-    writeYourGenres: function(){
-        for (let i = 1; i <= 3; i++) { 
-            personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, ''); 
-        }
-    },
     toggleVisibleMyDB: function(){
         if (personalMovieDB.privat) {
             personalMovieDB.privat = false;
         } else {
             personalMovieDB.privat = true;
         }
+    },
+    writeYourGenres: function(){
+        for (let i = 1; i <= 3; i++) { 
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+            if (genre == '' || genre === null) {
+                console.log('Вы ввели некорректные данные');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i+1} - это ${item}`);
+        });
     }
 };
 
